2feee75fa8487a8877d6f2ad806040f14bc0b138
Ewald Zietsman <ewald.zietsman@gmail.com>
2011-11-02 20:12:30 +0200

    TG* contains the exercises and eocexercises plus a template solutions environment with dummy solutions

--- Grade10/TeachersGuide/strip_books.py
+++ Grade10/TeachersGuide/strip_books.py
@@ -7,19 +7,20 @@ import sys, os
 
 
 def make_solutions(number):
-    solution = '''\n\n \\begin{solutions}
-\\begin{enumerate}[itemsep=5pt, label=\\textbf{(\\alph*)}]
+    items = ''.join(['\\item solution %s\n'%(i+1) for i in range(number)])
+    solution = '''\n\n \\begin{solutions}{}{
+\\begin{enumerate}[itemsep=5pt, label=\\textbf{\\arabic*}. ] 
 
-\LARGE Enter solutions here!!!!!
 
-\\end{enumerate}
+%s
+\\end{enumerate}}
 \\end{solutions}\n\n
-'''
+'''%(items)
     return solution
 
 def strip_file(lines):
     inside_ex = False
-    inside_first_enumerate = False 
+    no_enumi = 0 
     no_of_questions = 0
     stripped = []
 
@@ -27,33 +28,11 @@ def strip_file(lines):
         if (len(line.strip()) > 0):
             if (line.strip()[0] != '%'):
                 
-                # count solutions
-                if inside_ex:
-                    if ('\\begin{enumerate}' in line) and (inside_first_enumerate == False):
-                        inside_first_enumerate = True
-                    
-                    if ('\\begin{enumerate}' in line) and (inside_first_enumerate == True):
-                        inside_first_enumerate = False
-
-                    if ('\\end{enumerate}' in line) and (inside_first_enumerate == True):
-                        pass
-
-                    if ('\\end{enumerate}' in line) and (inside_first_enumerate == False):
-                        inside_first_enumerate = True
-
-                    
-                if inside_first_enumerate:
-                    if '\\item' in line:
-                        no_of_questions += 1 
-
-                if '\\begin{exercises}' in line:
+                if ('\\begin{exercises}' in line) or ('\\begin{eocexercises}' in line):
+                    no_of_questions = 0
+                    no_enumi = 0
                     inside_ex = True
 
-                if '\\end{exercises}' in line:
-                    stripped.append(line)
-                    inside_ex = False
-                    stripped.append(make_solutions(no_of_questions))
-                    no_of_questions = 0
 
                 if any([r in line for r in ['\\section{', '\\subsection{', '\\chapter{']]):
                     stripped.append(line)
@@ -61,20 +40,33 @@ def strip_file(lines):
                 if inside_ex:
                     stripped.append(line)
 
+                # count solutions
+                    if '{enumerate}' in line:
+                        no_enumi += 1
+
+                    if '\\item' in line:
+                        if no_enumi % 2 > 0:
+                            no_of_questions += 1
+                
+                    if ('\\end{exercises}' in line) or ('\\end{eocexercises}' in line):
+                        inside_ex = False
+                        stripped.append(make_solutions(no_of_questions))
+    
+    
     return stripped
 
 
 
 
 if __name__ == "__main__":
-    mathsfiles = [f.strip() for f in os.listdir(os.curdir) if 'Gr10-' == f[0:5]]
-    for f in mathsfiles:
-        outfile = open('TG_' + f, 'w')
-        print f
-        text = open(f, 'r').readlines()
+    #mathsfiles = [f.strip() for f in os.listdir(os.curdir) if 'Gr10-' == f[0:5]]
+    #for f in mathsfiles:
+        #outfile = open('Temp_' + f, 'w')
+        #print f
+        #text = open(f, 'r').readlines()
         # strip out everything that is not an exercise or eocexercise
-        stripped = strip_file(text)
-        outfile.write(''.join(stripped))
-        outfile.close()
-
+        #stripped = strip_file(text)
+        #outfile.write(''.join(stripped))
+        #outfile.close()
+    print '\n\nDo not run this if you do not know what you are doing\n\n'
 
